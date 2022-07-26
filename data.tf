ee4775ebe7976cc564a37d93b327068c482d0f7a
Lukas Nagel <lukas@lna-dev.net>
2022-07-26 10:52:24 +0200

    added comment to data

--- data.tf
+++ data.tf
@@ -1,3 +1,5 @@
+# TODO May break if a module with an older version uses the newest scripts
+
 data "http" "KubeInitMaster" {
   url = "https://raw.githubusercontent.com/LNA-DEV/terraform-hetzner-kubernetes/main/Scripts/KubeInitMaster.sh"
 }
