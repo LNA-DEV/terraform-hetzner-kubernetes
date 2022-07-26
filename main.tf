a7800e7e90303a1df12821a33f7ae26c4701298e
Lukas Nagel <lukas@lna-dev.net>
2022-07-25 21:58:56 -0600

    Init🎆

---
+++ main.tf
@@ -0,0 +1,12 @@
+terraform{
+    required_providers {
+    hcloud = {
+      source  = "hetznercloud/hcloud"
+      version = "1.34.3"
+    }
+    }
+}
+
+provider "hcloud" {
+  token = var.hcloud_token
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "hcloud_token" {
+  sensitive   = true # Requires terraform >= 0.14
+  type        = string
+  description = "Hetzner API-Token"
+}
