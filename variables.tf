b2d1b7f2a390a7730a7d8af2491d5dc4f4fef72a
Lukas Nagel <lukas@lna-dev.net>
2022-07-26 11:22:55 +0200

    updated variables

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "hcloud_token" {
-  sensitive   = true # Requires terraform >= 0.14
+  sensitive   = true
   type        = string
-  description = "Hetzner API-Token"
+  description = "Hetzner API-Token for the project you want the resources to be on"
 }
 
 variable "kubeNodeCount" {
   type        = number
-  description = "Count of Kubernetes Nodes"
+  description = "Count of Kubernetes Nodes which are added in addition to the master node"
   default     = 1
 }
