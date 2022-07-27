cd95f8429115faabc6fc1e7098156ebf98919d6e
Lukas Nagel <lukas@lna-dev.net>
2022-07-26 22:26:23 -0600

    added load balancer

--- kubernetes.tf
+++ kubernetes.tf
@@ -46,3 +46,30 @@ resource "hcloud_network_subnet" "kubeNetworkSubnet" {
   network_zone = "eu-central"
   ip_range     = "10.0.0.0/16"
 }
+
+# Load Balancer
+resource "hcloud_load_balancer" "load_balancer" {
+    name               = "load_balancer"
+  load_balancer_type = "lb11"
+  location           = "nbg1"
+  target {
+    type      = "server"
+    server_id = hcloud_server.kubeNode["0"].id
+  }
+}
+
+resource "hcloud_load_balancer_service" "load_balancer_service" {
+    load_balancer_id = hcloud_load_balancer.load_balancer.id
+    protocol         = "https"
+    destination_port = "30001"
+
+    http {
+        certificates = [hcloud_managed_certificate.managed_cert.id]
+        redirect_http = true
+    }
+}
+
+resource "hcloud_managed_certificate" "managed_cert" {
+  name         = "managed_cert"
+  domain_names = ["*.lna-dev.net"]
+}
