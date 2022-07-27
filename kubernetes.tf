be7beb0e955293313e29c28a5296fca78c7e76f1
Lukas Nagel <lukas@lna-dev.net>
2022-07-27 10:53:43 +0200

    arranged file structure

--- kubernetes.tf
+++ kubernetes.tf
@@ -1,3 +1,5 @@
+# Kubernetes
+
 resource "hcloud_server" "kubeMaster" {
   name        = "kubeMaster"
   server_type = "cpx11"
@@ -45,41 +47,4 @@ resource "hcloud_network_subnet" "kubeNetworkSubnet" {
   network_id   = hcloud_network.kubeNetwork.id
   network_zone = "eu-central"
   ip_range     = "10.0.0.0/16"
-}
-
-# Load Balancer
-resource "hcloud_load_balancer" "load_balancer" {
-  name               = "load_balancer"
-  load_balancer_type = "lb11"
-  location           = "nbg1"
-}
-
-resource "hcloud_load_balancer_target" "load_balancer_target" {
-  type             = "server"
-  load_balancer_id = hcloud_load_balancer.load_balancer.id
-  server_id        = hcloud_server.kubeNode["0"].id
-}
-
-resource "hcloud_load_balancer_service" "load_balancer_service" {
-  load_balancer_id = hcloud_load_balancer.load_balancer.id
-  protocol         = "https"
-  destination_port = var.loadBalancerDestinationPort
-
-  health_check {
-    protocol = "tcp"
-    port     = var.loadBalancerDestinationPort
-    interval = 15
-    timeout  = 10
-    retries  = 3
-  }
-
-  http {
-    certificates  = [hcloud_managed_certificate.managed_cert.id]
-    redirect_http = true
-  }
-}
-
-resource "hcloud_managed_certificate" "managed_cert" {
-  name         = "managed_cert"
-  domain_names = var.certifacteDomains
 }
